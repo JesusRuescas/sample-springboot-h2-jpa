0f9cdc809db990d0117a98e4040b4448930450b7
Jesus Ruescas Junior <jesusruescasjunior@gmail.com>
2023-04-12 12:36:55 -0300

    Update SQL scripts

--- sample/src/main/resources/data.sql
+++ sample/src/main/resources/data.sql
@@ -1,3 +1,1 @@
-insert into sample_table (sample_name) values ('Sample name 1');
-insert into sample_table (sample_name) values ('Sample name 2');
-insert into sample_table (sample_name) values ('Sample name 3');
+select *
